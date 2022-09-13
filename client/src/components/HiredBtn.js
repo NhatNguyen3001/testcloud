294bc027381dd743be86d1cfccb6c8fc388824e0
NhatNguyen3001 <dangnhat54321@gmail.com>
2022-09-13 13:50:56 +0700

    test 1

--- client/src/components/HiredBtn.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HiredBtn = () => {
-  return (
-    <button className="btn btn-outline-info">Hired</button>
-  )
-}
-
-export default HiredBtn
--- client/src/customRouter/PrivateRouter.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from 'react-router-dom'
-
-const PrivateRouter = () => {
-    const firstLogin = localStorage.getItem('firstLogin')
-    return firstLogin ?  <Outlet /> : <Navigate to="/login" replace="true" />
-}
-
-export default PrivateRouter
